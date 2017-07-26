15001a19b429286e86cdb9fb8a3d9bf6191c110f
saipraveenkumar <sai_katteboina@simplicity-creations.com>
2017-07-26 18:23:29 +0530

    Initial Commit

---
+++ MangoSpinner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MangoSpinner
+//
+//  Created by Rushikesh Kulkarni on 26/07/17.
+//  Copyright © 2017 simplicity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MangoSpinner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MangoSpinner
+//
+//  Created by Rushikesh Kulkarni on 26/07/17.
+//  Copyright © 2017 simplicity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
